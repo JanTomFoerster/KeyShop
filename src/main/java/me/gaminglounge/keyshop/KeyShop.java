4e5f167c75bd90589bbb2398b3151602238d93a4
JanTomFoerster <148646942+JanTomFoerster@users.noreply.github.com>
2024-05-26 17:29:25 +0200

    First upload

---
+++ src/main/java/me/gaminglounge/keyshop/KeyShop.java
@@ -0,0 +1,17 @@
+package me.gaminglounge.keyshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KeyShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
