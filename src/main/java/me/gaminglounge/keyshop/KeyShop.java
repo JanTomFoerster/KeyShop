7f147a0daad91eb469d0966808bbe34beaa8998d
jonas <jonas.saslona@gmx.de>
2024-06-11 20:42:37 +0200

    fixed commadapi collisions

--- src/main/java/me/gaminglounge/keyshop/KeyShop.java
+++ src/main/java/me/gaminglounge/keyshop/KeyShop.java
@@ -25,7 +25,7 @@ public final class KeyShop extends JavaPlugin {
 
         this.logger = this.getLogger();
 
-        CommandAPI.onLoad(new CommandAPIBukkitConfig(this));
+        if (!CommandAPI.isLoaded()) CommandAPI.onLoad(new CommandAPIBukkitConfig(this));
         new GiveKeyFragments();
         new OpenMainGui();
     }
